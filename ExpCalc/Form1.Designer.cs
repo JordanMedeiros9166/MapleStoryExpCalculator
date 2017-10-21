96220c7e7633e3d4b6bb66540f8356ee06de6fd0
JordanMedeiros9166 <jordanmedeiros13@hotmail.com>
2017-10-21 14:43:22 -0400

    Add project files.

---
+++ ExpCalc/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace ExpCalc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.txtCurrLvl = new System.Windows.Forms.TextBox();
+            this.txtExpSample = new System.Windows.Forms.TextBox();
+            this.txtTotalExp = new System.Windows.Forms.TextBox();
+            this.txtTargetLvl = new System.Windows.Forms.TextBox();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(223, 111);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(150, 79);
+            this.btnCalc.TabIndex = 0;
+            this.btnCalc.Text = "Calculate!";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtCurrLvl
+            // 
+            this.txtCurrLvl.Location = new System.Drawing.Point(92, 12);
+            this.txtCurrLvl.Name = "txtCurrLvl";
+            this.txtCurrLvl.Size = new System.Drawing.Size(79, 20);
+            this.txtCurrLvl.TabIndex = 1;
+            // 
+            // txtExpSample
+            // 
+            this.txtExpSample.Location = new System.Drawing.Point(15, 170);
+            this.txtExpSample.Name = "txtExpSample";
+            this.txtExpSample.Size = new System.Drawing.Size(129, 20);
+            this.txtExpSample.TabIndex = 2;
+            // 
+            // txtTotalExp
+            // 
+            this.txtTotalExp.Location = new System.Drawing.Point(15, 111);
+            this.txtTotalExp.Name = "txtTotalExp";
+            this.txtTotalExp.Size = new System.Drawing.Size(156, 20);
+            this.txtTotalExp.TabIndex = 3;
+            // 
+            // txtTargetLvl
+            // 
+            this.txtTargetLvl.Location = new System.Drawing.Point(92, 51);
+            this.txtTargetLvl.Name = "txtTargetLvl";
+            this.txtTargetLvl.Size = new System.Drawing.Size(79, 20);
+            this.txtTargetLvl.TabIndex = 4;
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Enabled = false;
+            this.txtOutput.Location = new System.Drawing.Point(223, 12);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(361, 81);
+            this.txtOutput.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(12, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Target level:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Location = new System.Drawing.Point(12, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Current level:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Location = new System.Drawing.Point(12, 95);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(125, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Total current experiance:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 154);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(156, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Current @epm (exp per minute):";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Green;
+            this.BackgroundImage = global::ExpCalc.Properties.Resources.ez;
+            this.ClientSize = new System.Drawing.Size(603, 225);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.txtTargetLvl);
+            this.Controls.Add(this.txtTotalExp);
+            this.Controls.Add(this.txtExpSample);
+            this.Controls.Add(this.txtCurrLvl);
+            this.Controls.Add(this.btnCalc);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "MapleStory v62 Calculator - Zeretius";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.TextBox txtCurrLvl;
+        private System.Windows.Forms.TextBox txtExpSample;
+        private System.Windows.Forms.TextBox txtTotalExp;
+        private System.Windows.Forms.TextBox txtTargetLvl;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ ExpCalc/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExpCalc
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtOutput.Text = "";
+            double currLvl = 0;
+            double tarLvl = 0;
+            double currExp = 0;
+            double expSmaple = 0;
+            string errMsg = "";
+
+            try
+            {
+                currLvl = double.Parse(txtCurrLvl.Text);
+                tarLvl = double.Parse(txtTargetLvl.Text);
+                currExp = double.Parse(txtTotalExp.Text);
+                expSmaple = double.Parse(txtExpSample.Text);
+            }
+            catch (Exception ex)
+            {
+                errMsg = "Please enter valid information in the textboxes." + Environment.NewLine;
+
+            }
+            double totalExp = 0;
+            double neededExp = 0;
+            double days = 0;
+            double hours = 0;
+            double minutes = 0;
+            double seconds = 0;
+            double totalHours = 0;
+
+            double arrExp = 0;
+            double arrExpCurr = 0;
+            int[] expInfo = {0, 15, 34, 57, 92, 135, 372, 560, 840, 1242, 1716, 2360, 3216, 4200, 5460, 7050, 8840,
+        11040, 13716, 16680, 20216, 24402, 28980, 34320, 40512, 47216, 54900, 63666, 73080, 83720, 95700, 108480,
+        122760, 138666, 155540, 174216, 194832, 216600, 240500, 266682, 294216, 324240, 356916, 391160, 428280, 468450,
+        510420, 555680, 604416, 655200, 709716, 748608, 789631, 832902, 878545, 926689, 977471, 1031036, 1087536,
+        1147032, 1209994, 1276301, 1346242, 1420016, 1497832, 1579913, 1666492, 1757815, 1854143, 1955750, 2062925,
+        2175973, 2295216, 2420993, 2553663, 2693603, 2841212, 2996910, 3161140, 3334370, 3517093, 3709829, 3913127,
+        4127566, 4353756, 4592341, 4844001, 5109452, 5389449, 5684790, 5996316, 6324914, 6671519, 7037118, 7422752,
+        7829518, 8258575, 8711144, 9188514, 9692044, 10223168, 10783397, 11374327, 11997640, 12655110, 13348610,
+        14080113, 14851703, 15665576, 16524049, 17429566, 18384706, 19392187, 20454878, 21575805, 22758159, 24005306,
+        25320796, 26708375, 28171993, 29715818, 31344244, 33061908, 34873700, 36784778, 38800583, 40926854, 43169645,
+        45535341, 48030677, 50662758, 53439077, 56367538, 59456479, 62714694, 66151459, 69776558, 73600313, 77633610,
+        81887931, 86375389, 91108760, 96101520, 101367883, 106992842, 112782213, 118962678, 125481832, 132358236,
+        139611467, 147262175, 155332142, 163844343, 172823012, 182293713, 192283408, 202820538, 213935103, 225658746,
+        238024845, 251068606, 264827165, 279339639, 294647508, 310794191, 327825712, 345790561, 364739883, 384727628,
+        405810702, 428049128, 451506220, 476248760, 502347192, 529875818, 558913012, 589541445, 621848316, 655925603,
+        691870326, 729784819, 769777027, 811960808, 856456260, 903390063, 952895838, 1005114529, 1060194805,
+        1118293480, 1179575962, 1244216724, 1312399800, 1384319309, 1460180007, 1540197871, 1624600714, 1713628833,
+        1807535693, 1906558648, 2011069705, 2121276324 };
+
+            if(expSmaple < 20) {
+                errMsg += "Please enter a valid epm sample!";
+            }
+            if (errMsg == "") {
+                if (tarLvl < 201 && tarLvl > currLvl)
+                {                    
+                
+                for (int i = 0; i < tarLvl; i++)
+                {              
+                        arrExp += expInfo[i];
+                
+                }
+                for (int i = 0; i < currLvl; i++)
+                {
+                        arrExpCurr += expInfo[i];
+
+                }
+
+                totalExp = arrExp;
+                neededExp = totalExp - (arrExpCurr + currExp);            
+                totalHours = (neededExp / (expSmaple * 60));
+                if (totalHours > 24)
+                {
+                    
+                    
+                    var timeSpan = TimeSpan.FromHours(totalHours);                       
+                    days = timeSpan.Days;    
+                    hours = timeSpan.Hours;
+                    minutes = timeSpan.Minutes;
+                    seconds = timeSpan.Seconds;               
+                    txtOutput.Text += "Total time: "+ days + " day(s) " + hours + " hour(s) " + minutes + " minute(s) " +  seconds + " second(s) " +Environment.NewLine;
+                }
+                else
+                {
+                    var timeSpan = TimeSpan.FromHours(totalHours);
+                    hours = timeSpan.Hours;
+                    minutes = timeSpan.Minutes;
+                    seconds = timeSpan.Seconds;
+                    txtOutput.Text += "Total time: " + hours + " hour(s) " + minutes + " minute(s) " + seconds + " second(s) " + Environment.NewLine;
+                } 
+                txtOutput.Text += "Total exp from level: " + currLvl + " to " + tarLvl +": "+ String.Format("{0:n0}", neededExp) + Environment.NewLine;
+                txtOutput.Text += "Exp per hour: " + String.Format("{0:n0}",(expSmaple * 60)) + Environment.NewLine;
+                }
+            }
+            else
+            {
+                txtOutput.Text = "Something went wrong!. Please re-enter information" + Environment.NewLine;
+                txtOutput.Text = errMsg;
+                    
+            }
+            
+        }
+    }
+}
